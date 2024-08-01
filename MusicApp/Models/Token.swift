9ccf71380ba81b6f05e09fc32f286f48294e765b
Miguel Sandoval <miguel_0799@hotmail.com>
2024-08-01 03:41:03 -0700

    feat: request token from spoitfy api

--- MusicApp/Models/Token.swift
+++ MusicApp/Models/Token.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class Token: Codable {
+    let accessToken: String
+    let tokenType: String
+    let expiresIn: Int 
+}
--- MusicApp/Utils/Auth.swift
+++ MusicApp/Utils/Auth.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct Auth {
+    static let shared: Auth = Auth()
+    
+    func requestToken() async {
+        let requestHeaders: [String:String] = ["Authorization": SPOTIFY_API_AUTH_KEY, "Content-Type": "application/x-www-form-urlencoded"]
+        
+        var requestBodyComponents = URLComponents()
+        requestBodyComponents.queryItems = [
+            URLQueryItem(name: "grant_type", value: "client_credentials"),
+            URLQueryItem(name: "redirect_uri", value: "google.com")
+        ]
+        
+        guard let url = (URL(string: SpotifyEndpoints.token.rawValue)) else {
+            print("URL didn't work")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = requestHeaders
+        request.httpBody = requestBodyComponents.query?.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let token = try decoder.decode(Token.self, from: data)
+                KeychainWrapper.standard.set(token.accessToken, forKey: "token")
+            } catch {
+                print("Error trying to decode")
+            }
+        }.resume()
+    }
+}
