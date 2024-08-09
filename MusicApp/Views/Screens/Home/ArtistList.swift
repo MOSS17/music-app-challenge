0e8f2b99830048ce0894ae7ed73b5b580d2a9d93
Miguel Sandoval <miguel_0799@hotmail.com>
2024-08-09 03:24:24 -0700

    handle error

--- MusicApp/Views/Screens/Home/ArtistList.swift
+++ MusicApp/Views/Screens/Home/ArtistList.swift
@@ -11,37 +11,44 @@ struct ArtistList: View {
     @Binding var artists: [Artist]
     
     var body: some View {
-        VStack(spacing: K.Spacing.content) {
-            ForEach(artists, id: \.id) { artist in
-                NavigationLink(destination: ArtistDetailsView(artist: .constant(artist))) {
-                    HStack(spacing: K.Spacing.content) {
-                        ImageView(urlString: artist.images.first?.url, backgroundColor: .constant(.primary))
-                            .frame(width: K.Size.listImage, height: K.Size.listImage)
-                            .cornerRadius(K.Size.cornerRadius)
-                        VStack(alignment: .leading, spacing: K.Spacing.inner) {
-                            Text(artist.name)
-                                .font(.title2)
-                                .bold()
-                            HStack {
-                                ForEach(artist.genres.prefix(K.Limit.genres), id: \.self) { genre in
-                                    GenreTag(genre: genre)
+        if artists.isEmpty {
+            Text("No artists available, try restarting the app")
+                .font(.title2)
+                .foregroundStyle(.white)
+                .padding()
+        } else {
+            VStack(spacing: K.Spacing.content) {
+                ForEach(artists, id: \.id) { artist in
+                    NavigationLink(destination: ArtistDetailsView(artist: .constant(artist))) {
+                        HStack(spacing: K.Spacing.content) {
+                            ImageView(urlString: artist.images.first?.url, backgroundColor: .constant(.primary))
+                                .frame(width: K.Size.listImage, height: K.Size.listImage)
+                                .cornerRadius(K.Size.cornerRadius)
+                            VStack(alignment: .leading, spacing: K.Spacing.inner) {
+                                Text(artist.name)
+                                    .font(.title2)
+                                    .bold()
+                                HStack {
+                                    ForEach(artist.genres.prefix(K.Limit.genres), id: \.self) { genre in
+                                        GenreTag(genre: genre)
+                                    }
                                 }
                             }
+                            Spacer()
                         }
-                        Spacer()
+                        .foregroundStyle(.white)
                     }
-                    .foregroundStyle(.white)
                 }
             }
+            .padding(.all)
         }
-        .padding(.all)
     }
 }
 
 #Preview {
     ZStack {
         GradientBackground(color: .blue)
-        ArtistList(artists: .constant(Artist.mockedData))
+        ArtistList(artists: .constant([]))
     }
 }
 
